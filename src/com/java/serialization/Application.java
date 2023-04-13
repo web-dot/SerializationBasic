d2d9d565a528b3fe001361b85eb57f55fb031b20
sudipto <cg.sudipc@gmail.com>
2023-04-13 23:04:13 +0530

    Serialiing object to and from a File using ObjectOutputStream and ObjectInputStream

--- src/com/java/serialization/Application.java
+++ src/com/java/serialization/Application.java
@@ -1,18 +1,27 @@
 package com.java.serialization;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
 public class Application {
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception {
 		
 		Player p1 = new Player("Ravi", 100);
 		
 		File f = new File("Player.txt");
 		FileOutputStream fos = new FileOutputStream(f);
+		/**ObjectOutputStream writes primitive data types and graphs of Java objects to an OutputStream*/
 		ObjectOutputStream oos = new ObjectOutputStream(fos);
 		oos.writeObject(p1);
+		
+		FileInputStream fis = new FileInputStream(f);
+		/**ObjectInputStream is a input stream that reads serialized objects from a file*/
+		ObjectInputStream ois = new ObjectInputStream(fis);
+		Player p2 = (Player) ois.readObject();
+		
+		System.out.println(p2.toString());
 	}
 }
--- src/com/java/serialization/Player.java
+++ src/com/java/serialization/Player.java
@@ -2,6 +2,12 @@ package com.java.serialization;
 
 import java.io.Serializable;
 
+/**
+ * -> To serialize a class, it must implement the Serializable interface.
+ * -> Serializable interface is a `marker interface` in Java.
+ * -> A marker interface do not have any methods or fields, thus the implementing class
+ *    do not have to implement any method.
+ * */
 public class Player implements Serializable {
 	private String name;
 	private int score;
@@ -20,6 +26,10 @@ public class Player implements Serializable {
 	}
 	public void setScore(int score) {
 		this.score = score;
+	}
+	@Override
+	public String toString() {
+		return "Player [name=" + name + ", score=" + score + "]";
 	}
 	
 }
